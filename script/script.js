ba4ab78a72d7aa054629a887280b30fcd9e26c4f
Ummah571 <samandaragzamov1@gmail.com>
2022-01-19 07:18:35 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,14 @@
+var a=+prompt("Введите число")
+var b=+prompt("Введите число")  
+var c=+prompt("Введите число")
+
+if (a>b,b<c) {
+    var d=b;
+    alert(d);
+}else if (a<b,a>c) {
+    var d=a;
+    alert(d);
+}else if (c<a,c>b) {
+    var d=c;
+    alert(d);
+}
